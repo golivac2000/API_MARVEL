3a35b25c93fcd83bdf362086bfaa9ab7089dd30b
golivac2000 <golivac2@miumg.edu.gt>
2023-10-08 22:52:53 -0600

    Inicio del proyecto

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import axios from 'axios'
+import cors from 'cors'
+import crypto from 'crypto-js'
+import nodemon from 'nodemon'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app= express()
+const puerto= process.env.PUERTO
+
+
+app.use(cors())
+app.use(express.json())
+
+// rutas backend
+
+app.get('/api/heroes', async (req, res)=> {
+    try{
+
+    }catch (error){
+    console.log(error)
+    }
+ }) 
+
+ app.get('/api/heroe/:id', async (req, res)=>{
+    try{
+
+    }catch (error){
+        console.log(error)
+    }
+ })
+
+
+
+
+app.listen(puerto, ()=>{
+    console.log(`Servidor backend corriendo en: http://localhost:${puerto}`)
+})
+
+
